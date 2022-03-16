f7acb2f6ce6f1a84c0760a338288328816fd7fa1
weed082 <forsythia3240@gmail.com>
2022-03-16 18:16:41 +0900

    [UPDATE] add prject card

--- pages/project.tsx
+++ pages/project.tsx
@@ -1,5 +1,25 @@
+import Layout from '../components/layouts/article'
+import { Grid, GridItem } from '@chakra-ui/react'
+import PrjCard from '../components/proj-card'
+
 const Project = () => {
-  return <span>poject</span>
+  const prjects = [
+    { idx: 1, url: 'url', title: 'title', desc: 'desc' },
+    { idx: 2, url: 'url', title: 'title', desc: 'desc' },
+    { idx: 3, url: 'url', title: 'title', desc: 'desc' }
+  ]
+  return (
+    <Layout>
+      <Grid templateColumns="repeat(2, 1fr)" gap={3}>
+        {prjects.map(prj => (
+          <GridItem key={prj.idx}>
+            {/* need to put youtube url  */}
+            <PrjCard url={prj.url} title={prj.title} desc={prj.desc}></PrjCard>
+          </GridItem>
+        ))}
+      </Grid>
+    </Layout>
+  )
 }
 
 export default Project
